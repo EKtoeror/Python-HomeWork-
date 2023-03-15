db4fd4c3f94dfab385838401d577f249d313b282
Rushan <ektoeror777@gmail.com>
2023-03-15 21:31:40 +0300

    Seminar_7

---
+++ Семинар_7q/Task_7-1.py
@@ -0,0 +1,12 @@
+stix = list(input().split())
+glasnie = {1: "АОУЫЭЕЁИЮЯаоуыэеёиюя"}
+summ = 0
+for i in range(0, len(stix)):
+    for j in stix[i]:
+        for k, v in glasnie.items():
+            if j in v:
+                summ += k
+if summ % len(stix) == 0:
+    print("Парам пам-пам")
+else:
+    print("Пам парам")
